efd92b2c08ed6f1259bc5d2e134bd0880ba1e8c5
sajibsaifullah <dusajibphy@gmail.com>
2023-03-28 14:51:58 +0600

    show data from cart

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 import "./Cart.css";
 
-const Cart = ({cart}) => {
-    // const cart = props.cart;  // option-1
-    // const { cart } = props;   // option-2
+const Cart = ({ cart }) => {
+  // const cart = props.cart;  // option-1
+  // const { cart } = props;   // option-2
 
-    console.log(cart);
+  console.log(cart);
 
-    let totalPrice = 0;
-    let totalShipping = 0;
-    for (const product of cart) {
-        // const productPrice = product.price;
-        totalPrice = totalPrice + product.price;
-        totalShipping = totalShipping + product.shipping;
-    };
-    const tax = totalPrice * 7 / 100;
-    const grandTotal = totalPrice + totalShipping + tax;
-    
+  let totalPrice = 0;
+  let totalShipping = 0;
+  let quantity = 0;
+  for (const product of cart) {
+    if (product.quantity === 0) {
+      product.quantity = 1;
+    }
+    // product.quantity = product.quantity || 1;
+    // const productPrice = product.price;
+    totalPrice = totalPrice + product.price * product.quantity;
+    totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
+  }
+  const tax = (totalPrice * 7) / 100;
+  const grandTotal = totalPrice + totalShipping + tax;
 
   return (
     <div className="cart">
       <h4>Order Summary</h4>
-          <p>Selected Items: {cart.length}</p>
-          <p>Total Price: $ {totalPrice}</p>
-          <p>Total Shipping Charge: $ {totalShipping}</p>
-          <p>Tax: $ {tax.toFixed(2)}</p>
-          <h6>Grand Total: $ {grandTotal.toFixed(2)}</h6>
+      <p>Selected Items: {quantity}</p>
+      <p>Total Price: $ {totalPrice}</p>
+      <p>Total Shipping Charge: $ {totalShipping}</p>
+      <p>Tax: $ {tax.toFixed(2)}</p>
+      <h6>Grand Total: $ {grandTotal.toFixed(2)}</h6>
     </div>
   );
 };
--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -34,10 +34,23 @@ const Shop = () => {
       // console.log('added product', addedProduct);
     }
     // step-5: set the cart
-    setCart(savedCart)
+    setCart(savedCart);
   }, [products]);
 
   const handleAddToCart = (product) => {
+   /*  let newCart = [];
+    // if product doesn't exist in the cart, then set quantity = 1
+    // if exist update quantity by 1
+    const exists = cart.find((pd) => pd.id === product.id);
+    if (!exists) {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    } else {
+      exists.quantity = exists.quantity + 1;
+      const remaining = cart.filter((pd) => pd.id !== product.id);
+      newCart = [...remaining, exists];
+    } */
+
     const newCart = [...cart, product];
     setCart(newCart);
     addToDb(product.id);
